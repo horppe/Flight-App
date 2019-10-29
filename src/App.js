506e8fdd4e5a2e996b826e688d8d404f3da84f1e
samsonoluokun <samson.oluokun@optisoft.ng>
2019-10-29 15:38:06 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/Login';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import Dashboard from './Pages/Dashboard/Dashboard';
 
 
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Redirect from="/" to="/login" />
         <Route path="/login" component={Login} />
         <Route path="/dashboard" component={Dashboard} />
       </Router>
